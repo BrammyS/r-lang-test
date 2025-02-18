fae753933196ea90dc6d04ff91f50b83a6ef5a51
Bram <bram.esendam@gmail.com>
2025-02-18 21:38:55 +0100

    Init

---
+++ pie-chart/pie-chart.R
@@ -0,0 +1,3 @@
+# Piechart example
+v <- c(10, 20, 30, 40)
+pie(v, col=rainbow(length(v)))
---
+++ plot/plot.R
@@ -0,0 +1,4 @@
+# Random scatter graph
+x <- rnorm(100)
+y <- rnorm(100)
+plot(x, y, main="Random Scatter Graph", xlab="X", ylab="Y")
